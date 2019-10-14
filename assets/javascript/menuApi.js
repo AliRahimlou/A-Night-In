83f19e178345363ac310199a47c25c23540f63f6
Nicholas Upshaw <nicholas.upsha@yahoo.com>
2019-10-14 11:21:41 -0400

    The Start of styling still needs some tweaks

--- assets/javascript/menuApi.js
+++ assets/javascript/menuApi.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 function recipeAPI() {
     var recipeApi = {
         "async": true,
@@ -17,14 +12,17 @@ function recipeAPI() {
     $.ajax(recipeApi).done(function (response) {
         console.log("food", response);
     });
-
+​
 }
-
-
+​
+​
+​
 var ingredients = [];
 var foodButtons = ["eggs", "milk", "bread", "cheese"]
-
-
+​
+​
+​
+​
 function renderButtons() {
     $("#commonFoodOptions").empty();
     for (var i = 0; i < foodButtons.length; i++) {
@@ -36,24 +34,99 @@ function renderButtons() {
     }
 }
 renderButtons();
-
-
-$(".foodOptionButtons").on("click", function() {
+​
+​
+$("#foodSearch").on("click", function(){
+    event.preventDefault();
+    var a = $("#addedIngredients").val().trim();
+    ingredients.push(a);
+    foodButtons.push(a);
+    console.log(ingredients);
+    recipeAPI();
+    renderButtons();
+    $("form")[0].reset();
+    
+});
+​
+$(document).on("click", ".foodOptionButtons", function() {
+    event.preventDefault();
     ingredients.push($(this).attr("data-state"));
     console.log("ing", ingredients)
     recipeAPI();
-
-
-
+    // $(this).remove()
 });
+ 
+function reset(){
+    ingredients= []
+    console.log(ingredients)
+}
+​
+​
+$("#reset").on("click", function(){
+    reset()
+})
+​
+​
+​
+​
+​
+​
+// ​
 
-$("#foodSearch").on("click", function(){
-    ingredients.push($("#addedIngredients").val());
-    console.log(ingredients);
-    recipeAPI();
+
+
+
+// function recipeAPI() {
+//     var recipeApi = {
+//         "async": true,
+//         "crossDomain": true,
+//         "url": "https://webknox-recipes.p.rapidapi.com/recipes/findByIngredients?number=5&ingredients="+ingredients,
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+//             "x-rapidapi-key": "43acb27062msh71014d2b6f89ea0p19fc0bjsn4d672d238f0f"
+//         }
+//     }
+//     $.ajax(recipeApi).done(function (response) {
+//         console.log("food", response);
+//     });
+
+// }
+
+
+// var ingredients = [];
+// var foodButtons = ["eggs", "milk", "bread", "cheese"]
+
+
+// function renderButtons() {
+//     $("#commonFoodOptions").empty();
+//     for (var i = 0; i < foodButtons.length; i++) {
+//         var a = $("<button>");
+//         a.addClass("foodOptionButtons");
+//         a.attr("data-state", foodButtons[i]);
+//         a.text(foodButtons[i]);
+//         $("#commonFoodOptions").append(a);
+//     }
+// }
+// renderButtons();
+
+
+// $(".foodOptionButtons").on("click", function() {
+//     ingredients.push($(this).attr("data-state"));
+//     console.log("ing", ingredients)
+//     recipeAPI();
+
+
+
+// });
+
+// $("#foodSearch").on("click", function(){
+//     ingredients.push($("#addedIngredients").val());
+//     console.log(ingredients);
+//     recipeAPI();
     
     
-});
+// });
 
 
 
